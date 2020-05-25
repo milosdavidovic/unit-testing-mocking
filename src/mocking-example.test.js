0083c852b4971c7c0d09ef88a59e174f8e90eeb6
Milos Davidovic <milos.davidovic@embracesbs.com>
2020-05-25 20:30:21 +0200

    added mock example

--- src/mocking-example.test.js
+++ src/mocking-example.test.js
@@ -1,5 +1,10 @@
 const playRound = require("./mocking-example");
 const coinFlipper = require("./utils/coinFlipper");
+const service = require("./utils/walletService");
+
+afterEach(() => {
+  jest.clearAllMocks();
+});
 
 test("when user wins, playRound will return true", () => {
   const originalFlipCoin = coinFlipper.flipACoin;
@@ -18,5 +23,16 @@ test("when user losses, playRound will return false", () => {
   const result = playRound("1", "heads", 100);
   // assert
   expect(result).toBe(false);
-  jest.clearAllMocks();
+});
+
+test("when user wins, createTransaction is called with double bet amount", () => {
+  // arrange
+  jest.spyOn(coinFlipper, "flipACoin");
+  coinFlipper.flipACoin.mockImplementation(() => "heads");
+  service.createTransaction = jest.fn();
+  // act
+  playRound("1", "heads", 100);
+  // assert
+  expect(service.createTransaction).toBeCalledTimes(1);
+  expect(service.createTransaction).toBeCalledWith("1", 200);
 });
